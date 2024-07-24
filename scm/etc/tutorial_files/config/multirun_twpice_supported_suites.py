a44b08e141438dc8ddc4de22daa2750ec1a76166
Michael Kavulich, Jr. <kavulich@ucar.edu>
2024-07-24 09:30:06 -0600

    Update tutorial case for new input file format

--- scm/etc/tutorial_files/config/multirun_twpice_supported_suites.py
+++ scm/etc/tutorial_files/config/multirun_twpice_supported_suites.py
@@ -1,3 +1,7 @@
-cases     = ["twpice"]
-suites    = ["SCM_GFS_v16", "SCM_GFS_v17_p8", "SCM_RAP", "SCM_RRFS_v1beta", "SCM_WoFS_v0", "SCM_HRRR"]
-namelists = []
+run_list = [\ 
+            # Supported suites for CCCPP Version 7 release
+            {"case": "twpice", "suite": "SCM_GFS_v17_p8"}, \
+            {"case": "twpice", "suite": "SCM_GFS_v17_HR3_RRTMGP"}, \
+            {"case": "twpice", "suite": "SCM_GFS_v16"}, \
+            {"case": "twpice", "suite": "SCM_WoFS_v0"}, \
+            {"case": "twpice", "suite": "SCM_RRFS_v1"}]
